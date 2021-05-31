07109e146b162074603326f2721b152301477979
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-05-31 22:00:50 +0100

    commit

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -3,14 +3,15 @@ import React, { Component } from 'react';
 class Movie extends Component {
   handleClick = (event) => {
     const movieSrc = event.target.src;
-    const movieTitle = this.props.movies.find(movie => movie.Poster == event.target.src).Title;
-    const movieYear = this.props.movies.find(movie => movie.Poster == event.target.src).Year;
+    const movieTitle = this.props.movies.find(movie => movie.Poster === event.target.src).Title;
+    const movieYear = this.props.movies.find(movie => movie.Poster === event.target.src).Year;
     this.props.selectMovie(movieSrc, movieTitle, movieYear);
   }
 
   render() {
     return (
       <img
+        alt=""
         src={this.props.src}
         className='movie'
         onClick={this.handleClick}
