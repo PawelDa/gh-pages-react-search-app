fdbfd066d3b7072b8cccf08b86909bf9bce8f38e
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-06-06 22:03:43 +0100

    bootstraping redux to app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
 import './application.scss';
 import reportWebVitals from './reportWebVitals';
 
 import App from './components/app.jsx';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
