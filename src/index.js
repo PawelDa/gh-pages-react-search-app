9b15ef808826dd2d40e18a52d2b8c4c939cc0255
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-06-07 23:58:08 +0100

    commented redux imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+// import { Provider } from 'react-redux';
+// import { createStore, combineReducers } from 'redux';
 
 import './application.scss';
 import reportWebVitals from './reportWebVitals';
