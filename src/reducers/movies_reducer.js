c4047850827189034f812c44214e0d10f7bf9b38
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-06-09 23:58:52 +0100

    added switch statement

--- src/reducers/movies_reducer.js
+++ src/reducers/movies_reducer.js
@@ -3,10 +3,11 @@ const moviesReducer = (state, action) => {
     return []
   }
 
-  if (action.type === 'SET_MOVIES') {
-    return action.payload;
-  } else {
-    return state;
+  switch (action.type) {
+    case 'SET_MOVIES':
+      return action.payload;
+    default:
+      return state;
   }
 }
 
