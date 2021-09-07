4e7fa4ba71ea9800f0a8b06034c82c2bbe03337a
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-07 07:09:07 +0100

    changed App to function component

--- src/App.js
+++ src/App.js
@@ -1,90 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import MovieList from './components/movie_list';
 import SearchBar from './components/search_bar';
 import MovieDetails from './components/movie_details';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className='left-side'>
-          <SearchBar />
-          <MovieDetails />
-        </div>
-        <div className='right-side'>
-          <MovieList />
-        </div>
-      </div>
-    );
-  }
-}
-
-export default App;
-
-/*
---ONLY REACT--
-import React, { Component } from 'react';
-
-import MovieList from '../containers/movie_list';
-import SearchBar from './search_bar';
-import Movie from './movie';
-import MovieDetails from './movie_details';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      movies: [],
-      selectedMovie: {
-        src: null, //'https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_SX300.jpg',
-        title: null, //'Harry Potter and the Half-Blood Prince',
-        year: null //'2009'
-      }
-    }
-
-    this.search("x-men");
-  }
-
-  search = (query) => {
-    const apiKey = '48727053'
-    const url = `https://www.omdbapi.com/?s=${query}&apikey=${apiKey}`
-    fetch(url).then(response => response.json()).then((data) => {
-      const movies = data.Search
-      this.setState({
-        movies: movies
-      })
-    })
-  }
-
-  selectMovie = (movieSrc, movieTitle, movieYear) => {
-    this.setState({
-      selectedMovie: {
-        src: movieSrc,
-        title: movieTitle,
-        year: movieYear
-      }
-    })
-  }
-
-  render() {
-    return (
-    <div>
-      <div className='left-side'>
-        <SearchBar searchFunction={this.search} />
-        <div className='selected-movie card'>
-          <Movie src={this.state.selectedMovie.src} className='card-img-top'/>
-          <MovieDetails title={this.state.selectedMovie.title} year={this.state.selectedMovie.year} />
-        </div>
-      </div>
-      <div className='right-side'>
-        <MovieList movies={this.state.movies} selectMovie={this.selectMovie} />
-      </div>
+const App = () => (
+  <div>
+    <div className='left-side'>
+      <SearchBar />
+      <MovieDetails />
     </div>
-    )
-  }
-}
+    <div className='right-side'>
+      <MovieList />
+    </div>
+  </div>
+);
 
 export default App;
-*/
