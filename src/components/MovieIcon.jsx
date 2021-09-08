af82f5b07561727d8ec5574c43c6d1a6ba81496e
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-08 20:21:44 +0100

    added bootstrap styling

--- src/components/MovieIcon.jsx
+++ src/components/MovieIcon.jsx
@@ -6,15 +6,11 @@ const MovieIcon = ({ movie }) => {
   const url = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
   console.log(url)
   return (
-    <div className='movie-icon'>
-      <div
-        className='background-image'
-        style={{
-          backgroundImage: `url(${url})`  
-        }}
-      />
-      <div className='content'>
-        <span className='title'>{movie.title}</span>
+    <div className='card'>
+      <img src={url} className='card-img-top' />
+      <div className='card-body just'>
+        <h5 className='card-title'>{movie.title}</h5>
+        <a href="#" className="btn btn-primary">Movie Details</a>
       </div>
     </div>
   )
