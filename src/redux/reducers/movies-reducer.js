e3ae3346275c2d0814893ea52e602940daebac1a
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-08 18:42:54 +0100

    change initial state

--- src/redux/reducers/movies-reducer.js
+++ src/redux/reducers/movies-reducer.js
@@ -1,19 +1,15 @@
 import { SET_MOVIES } from "../actions/types";
 
-const initialState = {
-  movies: []
-};
+const initialState = [];
+
 
 const moviesReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_MOVIES:
-      return {
-        ...state,
-        movies: action.payload
-      };
+      return action.payload;
     default:
       return state;
   }
-}
+};
 
 export default moviesReducer;
