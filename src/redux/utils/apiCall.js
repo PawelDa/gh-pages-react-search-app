b840fbe86df4654118b399eb218ab803f7186250
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-09 19:56:01 +0100

    env variables added

--- src/redux/utils/apiCall.js
+++ src/redux/utils/apiCall.js
@@ -1,7 +1,6 @@
 export async function setMoviesList(searchedQuery) {
   const apiKey = process.env.REACT_APP_API_KEY;
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchedQuery}`
-  
   try {
     const response = await fetch(url);
     const data = await response.json();
